c1bffd4ef90f6ce4c4a1f7553cdf78fcb23be67b
Allyssa May <allyssamayocogcocristino@gmail.com>
2026-05-12 21:00:11 +0800

    Documentation

--- Codes/SpeedyEnemy.java
+++ Codes/SpeedyEnemy.java
@@ -4,14 +4,16 @@ public class SpeedyEnemy extends Enemy{
 
     public SpeedyEnemy(int x, int y, int panelWidth, int panelHeight){
         super(x, y, panelWidth, panelHeight);
-        setSpeed(5);
+        setSpeed(5); // Set higher speed value compared to default enemies
 
         // Load speedy-specific sprites once
+        // Movement
         walkUp    = loadStrip("Entities/Enemy/Walk/Speedy/speedy_up", 8);
         walkDown  = loadStrip("Entities/Enemy/Walk/Speedy/speedy_down", 8);
         walkLeft  = loadStrip("Entities/Enemy/Walk/Speedy/speedy_left", 8);
         walkRight = loadStrip("Entities/Enemy/Walk/Speedy/speedy_right", 8);
 
+        // Combat
         attackUp    = loadStrip("Entities/Enemy/Attack/Speedy/speedy_attack_up", 6);
         attackDown  = loadStrip("Entities/Enemy/Attack/Speedy/speedy_attack_down", 6);
         attackLeft  = loadStrip("Entities/Enemy/Attack/Speedy/speedy_attack_left", 6);
