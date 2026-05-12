b0d6fd7393c6991aeabcc16e83409e4bd7f81566
MattL <mdlaraga@up.edu.ph>
2026-05-12 15:01:12 +0800

    loaded Boss and Minion

--- Codes/BossEnemy.java
+++ Codes/BossEnemy.java
@@ -16,6 +16,17 @@ public class BossEnemy extends Enemy{
         setHealth(67);
         width = width * 2;
         height = height * 2;
+        
+        // Load boss-specific sprites once
+        walkUp    = loadStrip("Entities/Enemy/Walk/Minion/minion_up", 8);
+        walkDown  = loadStrip("Entities/Enemy/Walk/Minion/minion_down", 8);
+        walkLeft  = loadStrip("Entities/Enemy/Walk/Minion/minion_left", 8);
+        walkRight = loadStrip("Entities/Enemy/Walk/Minion/minion_right", 8);
+
+        attackUp    = loadStrip("Entities/Enemy/Attack/Minion/minion_attack_up", 6);
+        attackDown  = loadStrip("Entities/Enemy/Attack/Minion/minion_attack_down", 6);
+        attackLeft  = loadStrip("Entities/Enemy/Attack/Minion/minion_attack_left", 6);
+        attackRight = loadStrip("Entities/Enemy/Attack/Minion/minion_attack_right", 6);
     }
 
     public List<Enemy> spawnEnemies(){
