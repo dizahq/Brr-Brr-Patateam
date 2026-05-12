c36fe9dfc17cd26420871bebb2d5a453330b0483
Allyssa May <allyssamayocogcocristino@gmail.com>
2026-05-12 20:58:07 +0800

    Documentation

--- Codes/TankyEnemy.java
+++ Codes/TankyEnemy.java
@@ -4,14 +4,16 @@ public class TankyEnemy extends Enemy{
 
     public TankyEnemy(int x, int y, int panelWidth, int panelHeight){
         super(x, y, panelWidth, panelHeight);
-        setHealth(2);
+        setHealth(2); // Harder to kill
 
         // Load specific sprites once
+        // Movement 
         walkUp    = loadStrip("Entities/Enemy/Walk/Tank/tank_up", 8);
         walkDown  = loadStrip("Entities/Enemy/Walk/Tank/tank_down", 8);
         walkLeft  = loadStrip("Entities/Enemy/Walk/Tank/tank_left", 8);
         walkRight = loadStrip("Entities/Enemy/Walk/Tank/tank_right", 8);
 
+        // Combat
         attackUp    = loadStrip("Entities/Enemy/Attack/Tank/tank_attack_up", 6);
         attackDown  = loadStrip("Entities/Enemy/Attack/Tank/tank_attack_down", 6);
         attackLeft  = loadStrip("Entities/Enemy/Attack/Tank/tank_attack_left", 6);
