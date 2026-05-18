41f68d3dc5a1eac2f49f2f5e0b04f8e6c568942c
Allyssa May <allyssamayocogcocristino@gmail.com>
2026-05-18 21:39:30 +0800

    Improved

--- TheLastStand/src/ui/HelpPanel.java
+++ TheLastStand/src/ui/HelpPanel.java
@@ -1,20 +1,35 @@
 package ui;
 
-import java.awt.BorderLayout;
+import java.awt.Graphics;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import java.awt.Image;
+import javax.swing.ImageIcon;
 
 public class HelpPanel extends OverlayPanel{
-    private JPanel help = getContainerPanel(); 
+    private Image helpImage;
+    private int size = 600;
+    private int panelWidth;
+    private int panelHeight;
     public HelpPanel(int panelWidth, int panelHeight){
         super(panelWidth, panelHeight, false);
+        this.panelWidth = panelWidth;
+        this.panelHeight = panelHeight;
         setVisible(false);
 
-        JLabel movementLabel = new JLabel("Movement: W, A, s, D");
-        JLabel shootingLabel = new JLabel("Shoot: Directional Keys");
+        helpImage = new ImageIcon("TheLastStand/assets/interface/mainMenu/help1.png").getImage();
 
-        help.setSize(500, 400);
-        help.setLayout(new BorderLayout());
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e){
+                setVisible(false);
+            }
+        });
+    }
+    @Override
+    protected void paintComponent(Graphics g){
+        super.paintComponent(g);
+        g.drawImage(helpImage, (panelWidth/2)-(size/2), (panelHeight/2)-(size/2), size, size, null);
     }
 }
