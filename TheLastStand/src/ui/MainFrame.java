dcf34cbb81f8f84704d51a53a891a204bb34aac5
Dizah Catenza <dqcatenza@up.edu.ph>
2026-05-21 12:23:21 +0800

    Merge

--- TheLastStand/src/ui/MainFrame.java
+++ TheLastStand/src/ui/MainFrame.java
@@ -14,7 +14,9 @@ public class MainFrame extends JFrame{
     
     public MainFrame(){
         // System initialization: ensures the save file is in clean state at launch
-        // SaveManager.resetFile(); // Use only if for every run, all game data will be reset for a clean state.
+        if (!SaveManager.hasSave()) {
+            SaveManager.resetFile();
+        }
         
         // Window configuration
         setBounds(0, 0, frameWidth, frameHeight); // Match screen size
